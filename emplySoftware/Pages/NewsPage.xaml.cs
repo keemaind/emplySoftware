aa4baedb4cebadece18daeaab3e091fb7595fb00
Kamil <Kamil@DESKTOP-LJHMLEH>
2024-06-06 23:27:16 +0300

    Сделал все

---
+++ emplySoftware/Pages/NewsPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿
+using emplySoftware.Class;
+using emplySoftware.DatabaseSQL;
+using emplySoftware.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace emplySoftware.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для NewsPage.xaml
+    /// </summary>
+    public partial class NewsPage : Page
+    {
+        List<News> lsview_news = new List<News>();
+        public NewsPage()
+        {
+            InitializeComponent();
+            var neews = App.ContextDatabase.News.ToList();
+            foreach (var neew in neews)
+            {
+                lsview_news.Add(new News
+                {
+                    Title = neew.Title,
+                    IMAGE = neew.IMAGE,
+                    CreateDate = neew.CreateDate,
+                    Description = neew.Description,
+                }
+                );
+            }
+            NewsListView.ItemsSource = lsview_news;
+
+        }
+
+        private bool UserFilterSearch(object item)
+        {
+            if (String.IsNullOrEmpty(search_text_box.Text))
+                return true;
+            else
+                return ((item as UserList).Login.IndexOf(search_text_box.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void search_text_box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(NewsListView.Items);
+            view.Filter = UserFilterSearch;
+        }
+            private void add_new_button_Click(object sender, RoutedEventArgs e)
+        {
+            NewsCreateWindow newsCreateWindow = new NewsCreateWindow();
+            newsCreateWindow.ShowDialog();
+        }
+
+        private void edit_new_button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedNews = NewsListView.SelectedItem as News;
+            if (selectedNews == null)
+            {
+                string error = "Выберите новость!";
+                ErrorWindow errorWindow = new ErrorWindow(error);
+                errorWindow.ShowDialog();
+            }
+            else
+            {
+                NewsCreateWindow newsCreateWindow = new NewsCreateWindow(selectedNews);
+                newsCreateWindow.ShowDialog();
+            }
+        }
+
+        private void delete_new_button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedNews = NewsListView.SelectedItem as News;
+            if (selectedNews == null)
+            {
+                string error = "Выберите новость!";
+                ErrorWindow errorWindow = new ErrorWindow(error);
+                errorWindow.ShowDialog();
+            }
+            else
+            {
+                YesOrNoWindow yesOrNoWindow = new YesOrNoWindow();
+                yesOrNoWindow.ShowDialog();
+                bool choice = yesOrNoWindow.choice;
+                if (choice)
+                {
+                    App.ContextDatabase.News.Remove(selectedNews);
+                    App.ContextDatabase.SaveChanges();
+                    string notification = "Новость удалена";
+                    NotificationWindow notificationWindow = new NotificationWindow(notification);
+                    notificationWindow.ShowDialog();
+                }
+                else
+                {
+
+                }
+            }
+           
+        }
+
+    }
+}
---
+++ emplySoftware/Windows/NewsCreateWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using emplySoftware.DatabaseSQL;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace emplySoftware.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для NewsCreateWindow.xaml
+    /// </summary>
+    public partial class NewsCreateWindow : Window
+    {
+        private byte[] _mainImageData;
+        private bool Im = false;
+        News news = null;
+        public NewsCreateWindow()
+        {
+            InitializeComponent();
+            Create.Visibility = Visibility.Visible;
+            Edit.Visibility = Visibility.Collapsed;
+        }
+        public NewsCreateWindow(News selectedNews)
+        {
+            InitializeComponent();
+            news = selectedNews;
+            CrOrEd.Content = "Редактирование новости";
+            TitleNews.Text = news.Title;
+            NewsTextBox.Text = news.Description;
+            Image.ImageSource = (ImageSource)new ImageSourceConverter().ConvertFrom(news.IMAGE);
+            Create.Visibility = Visibility.Collapsed;
+            Edit.Visibility = Visibility.Visible;
+        }
+        private void BtnSelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image |*.png; *.jpg; *.jpeg";
+            if (ofd.ShowDialog() == true)
+            {
+                _mainImageData = File.ReadAllBytes(ofd.FileName);
+                Image.ImageSource = (ImageSource)new ImageSourceConverter().ConvertFrom(_mainImageData);
+                Im = true;
+            }
+
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            if (TitleNews.Text == "" || NewsTextBox.Text == "")
+            {
+                string errorMessage = "Поле пустое!";
+                ErrorWindow errorWindow = new ErrorWindow(errorMessage);
+                errorWindow.ShowDialog();
+
+            }
+            else
+            {
+                if (_mainImageData == null)
+                {
+                    var theNews = new News
+                    {
+                        CreateDate = DateTime.Now,
+                        Title = TitleNews.Text,
+                        Description = NewsTextBox.Text,
+
+                    };
+                    App.ContextDatabase.News.Add(theNews);
+                    App.ContextDatabase.SaveChanges();
+                }
+                else
+                {
+                    var theNews = new News
+                    {
+                        CreateDate = DateTime.Now,
+                        Title = TitleNews.Text,
+                        Description = NewsTextBox.Text,
+                        IMAGE = _mainImageData,
+                    };
+                    App.ContextDatabase.News.Add(theNews);
+                    App.ContextDatabase.SaveChanges();
+                }
+
+            }
+
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (TitleNews.Text == "" || NewsTextBox.Text == "")
+            {
+                string errorMessage = "Поле пустое!";
+                ErrorWindow errorWindow = new ErrorWindow(errorMessage);
+                errorWindow.ShowDialog();
+
+            }
+            else
+            {
+                if (_mainImageData == null)
+                {
+                    news.Title = TitleNews.Text;
+                    news.Description = NewsTextBox.Text;
+                    App.ContextDatabase.SaveChanges();
+                }
+                else
+                {
+                    news.Title = TitleNews.Text;
+                    news.Description = NewsTextBox.Text;
+                    news.IMAGE = _mainImageData;
+                    App.ContextDatabase.SaveChanges();
+                }
+
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
